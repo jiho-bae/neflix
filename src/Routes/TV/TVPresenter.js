659881e6c27d942ece62ef264e0173bab8960ea6
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-03-18 20:36:13 +0900

    show datas using poster component on each screen.

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "Components/Loader";
 import Message from "Components/Message";
+import Poster from "Components/Poster";
 
 const Container = styled.div`
   padding: 0px 10px;
@@ -14,9 +15,51 @@ const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
     <Loader />
   ) : (
     <Container>
-      {topRated && topRated.length > 0 && <Section title="Top Rated Shows">{topRated.map((show) => show.name)}</Section>}
-      {popular && popular.length > 0 && <Section title="Popular Shows">{popular.map((show) => show.name)}</Section>}
-      {airingToday && airingToday.length > 0 && <Section title="Airing Today">{airingToday.map((show) => show.name)}</Section>}
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated Shows">
+          {topRated.map((show) => (
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date.substring(0, 4)}
+              isMovie={true}
+            />
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Shows">
+          {popular.map((show) => (
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date.substring(0, 4)}
+              isMovie={true}
+            />
+          ))}
+        </Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="Airing Today">
+          {airingToday.map((show) => (
+            <Poster
+              key={show.id}
+              id={show.id}
+              imageUrl={show.poster_path}
+              title={show.original_name}
+              rating={show.vote_average}
+              year={show.first_air_date.substring(0, 4)}
+              isMovie={true}
+            />
+          ))}
+        </Section>
+      )}
       {error && <Message color="#e74c3c" text={error} />}
     </Container>
   );
